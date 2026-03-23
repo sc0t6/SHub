303759ff2d2a8919fc96bc664b493196d6dd9e3d
scot <riftedkris@gmail.com>
2026-03-23 21:44:34 +0200

    Update SHub.lua

--- RayfieldUI/SHub.lua
+++ RayfieldUI/SHub.lua
@@ -13,100 +13,100 @@ local MainTab = Window:CreateTab("Script Hubs", 4483362458)
 local GamesTab = Window:CreateTab("Games", 125094830280177)
 local UniversalTab = Window:CreateTab("Universal", 10740274875) 
 
-MainTab:CreateSector("This is not in any order of best to worst.")
+local Section = MainTab:CreateSector("This is not in any order of best to worst.")
     
 
-MainTab:CreateButton({
+local Button = MainTab:CreateButton({
     Name = "Forge Hub",
     Callback = function(v)
         v = loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d5ed1fbd4301b1d18d75153c5b47181d.lua"))()
     end
 })
 
-MainTab:CreateButton({
+local Button = MainTab:CreateButton({
     Name = "Moondiety",
     Callback = function(v)
         v = loadstring(game:HttpGet("https://raw.githubusercontent.com/m00ndiety/Moondiety/refs/heads/main/Loader"))()
     end
 })
 
-MainTab:CreateButton({
+local Button = MainTab:CreateButton({
     Name = "Lumin Hub",
     Callback = function(v)
         v = loadstring(game:HttpGet("http://luminon.top/loader.lua"))()
     end
 })
 
-MainTab:CreateButton({
+local Button = MainTab:CreateButton({
     Name = "Axon Hub",
     Callback = function(v)
         v = loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/97c3f6db55a2cf72141537a85458e5a7.lua"))()
     end
 })
 
-MainTab:CreateButton({
+local Button = MainTab:CreateButton({
     Name = "Ather Hub",
     Callback = function(v)
         v = loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2529a5f9dfddd5523ca4e22f21cceffa.lua"))()
     end
 })
 
-MainTab:CreateButton({
+local Button = MainTab:CreateButton({
     Name = "Rift Hub",
     Callback = function(v)
         v = loadstring(game:HttpGet("https://rifton.top/loader.lua"))()
     end
 })
 
-UniversalTab:CreateSection("Admin")
+local Section = UniversalTab:CreateSection("Admin")
 
-UniversalTab:CreateButton({
+local Button = UniversalTab:CreateButton({
     Name = "Infinite Yield",
     Callback = function(v)
         v = loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
     end
 })
 
-UniversalTab:CreateButton({
+local Button = UniversalTab:CreateButton({
     Name = "Nameless Admin V1 (original)",
     Callback = function(a)
       a = loadstring(game:HttpGet("https://raw.githubusercontent.com/FilteringEnabled/NamelessAdmin/main/Source"))()
     end
 })
 
-UniversalTab:CreateButton({
+local Button = UniversalTab:CreateButton({
     Name = "Nameless Admin V2",
     Callback = function(a)
       a = loadstring(game:HttpGet("https://raw.githubusercontent.com/ltseverydayyou/Nameless-Admin/main/Source.lua"))()
     end
 })
 
-UniversalTab:CreateSector("More Scripts")
+local Section = UniversalTab:CreateSector("More Scripts")
 
-UniversalTab:CreateButton({
+local Button = UniversalTab:CreateButton({
     Name = "FScripts (Fun Scripts)",
     Callback = function(f)
       f = loadstring(game:HttpGet("https://raw.githubusercontent.com/sc0t6/FScripts/refs/heads/main/Source/sourcecode.lua"))()
     end
 })
 
-UniversalTab:CreateButton({
+local Button = UniversalTab:CreateButton({
     Name = "GTA Mod Menu",
     Callback = function(f)
       f = loadstring(game:HttpGet("https://vt-now.github.io/VIOLENT/vt.lua"))()
     end
 })
 
-GamesTab:CreateSection("Game Scripts")
+local Section = GamesTab:CreateSection("Game Scripts")
 
-GamesTab:CreateButton({
+local Button = GamesTab:CreateButton({
     Name = "99 Nights In The Forest Script",
     Callback = function(f)
         f = loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/VW-Add/main/nightsintheforest.lua", true))()
     end
 })
 
-GamesTab:CreateButton({
+local Button = GamesTab:CreateButton({
     Name = "Brookhaven",
     Callback = function(b)
         b = loadstring(game:HttpGet("https://raw.githubusercontent.com/nxvap/Hexagon/refs/heads/main/brookhaven"))()
